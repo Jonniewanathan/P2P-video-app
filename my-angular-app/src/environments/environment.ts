04dddd8391c8b02bbbf3884b14669c1bb84058ba
Jonathan Quirke <jonathan41@hotmail.com>
2025-02-21 14:40:12 +0000

    Updating deploy logic

---
+++ my-angular-app/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const config = {
+  production: false,
+  apiUrl: 'https://localhost:5000',
+  wsUrl: 'wss://localhost:5000',
+  firebaseConfig: {
+    apiKey: "YOUR_DEV_API_KEY",
+    authDomain: "YOUR_DEV_AUTH_DOMAIN",
+    projectId: "YOUR_DEV_PROJECT_ID",
+    storageBucket: "YOUR_DEV_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_DEV_MESSAGING_SENDER_ID",
+    appId: "YOUR_DEV_APP_ID",
+  }
+};
