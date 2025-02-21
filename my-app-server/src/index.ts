55354a2661e7506fc5e03f47d8b5c13b403ef0c9
Jonathan Quirke <jonathan41@hotmail.com>
2025-02-21 15:27:58 +0000

    Updating deploy logic and environment set up

--- my-app-server/src/index.ts
+++ my-app-server/src/index.ts
@@ -2,16 +2,25 @@ import express from 'express';
 import {Request, Response} from 'express';
 import * as admin from 'firebase-admin';
 import cors from 'cors';
-
-import serviceAccount from '../firebase-admin-key.json';
 import {ServiceAccount} from 'firebase-admin';
 import { WebSocketServer } from 'ws';
 import * as fs from 'node:fs';
 import path from 'node:path';
 import * as https from 'node:https';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+};
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as ServiceAccount),
+    credential: admin.credential.cert(firebaseConfig as ServiceAccount),
 });
 
 const db = admin.firestore();
