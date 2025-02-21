5c393c683eb66ba5fd5428912a9743df15e7c4bf
Jonathan Quirke <jonathan41@hotmail.com>
2025-02-21 20:07:22 +0000

    fixing incorrect cors settings

--- my-app-server/src/index.ts
+++ my-app-server/src/index.ts
@@ -11,9 +11,27 @@ import {ServiceAccount} from 'firebase-admin';
 import * as serviceAccount from '../firebase-admin-key.json';
 dotenv.config();
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as ServiceAccount),
-});
+// const firebaseConfig = {
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.FIREBASE_APP_ID,
+// };
+
+if (process.env.FIREBASE_PRIVATE_KEY ) {
+    const serviceAccountJson = JSON.parse(process.env.FIREBASE_PRIVATE_KEY as string);
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccountJson as ServiceAccount),
+    });
+} else {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount as ServiceAccount),
+    });
+}
+
+
 
 const db = admin.firestore();
 const app = express();
