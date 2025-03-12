d53ec167e4bdd9007524e6828eaa48a6b3f96d85
Jonathan Quirke <jonathan41@hotmail.com>
2025-03-12 12:08:35 +0000

    Updating file logic for certs and fixing registration and login

--- my-angular-app/src/environments/environment.ts
+++ my-angular-app/src/environments/environment.ts
@@ -3,11 +3,11 @@ export const config = {
   apiUrl: 'https://localhost:5000',
   wsUrl: 'wss://localhost:5000',
   firebase: {
-    apiKey: "AIzaSyBJpIQyO7PV57gyzUAFrip-hitgwmIiRe4",
-    authDomain: "p2p-video-conferencing-a4814.firebaseapp.com",
-    projectId: "p2p-video-conferencing-a4814",
-    storageBucket: "p2p-video-conferencing-a4814.firebasestorage.app",
-    messagingSenderId: "877642163912",
-    appId: "1:877642163912:web:c5f9562082edaceda567fd"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
